e54d1ab7d238e8debda40ee2dd1319d1c5e37872
Patil Tushar <patiltushar2101@gmail.com>
2021-04-29 02:48:20 +0530

    added my work

---
+++ src/services/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBQ22gyy4KJjwG_Ri951eJBLGGvc805vWI",
+  authDomain: "my-store-40735.firebaseapp.com",
+  projectId: "my-store-40735",
+  storageBucket: "my-store-40735.appspot.com",
+  messagingSenderId: "1043295789770",
+  appId: "1:1043295789770:web:8b948e9e9da1e736198a33",
+  measurementId: "G-SDLN6GWZLN"
+};
+
+export default firebaseConfig;
